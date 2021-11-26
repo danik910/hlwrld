5bd0bd5d3f19a242d9fec7f9686de42ce8564e2d
Danik <mevioly@gmail.com>
2021-11-26 12:51:02 +0300

    Tretye zadanie

--- hlwrld/main.cpp
+++ hlwrld/main.cpp
@@ -9,28 +9,35 @@
 //#include <vector>
 //#include <map>
 #include <string>
-
+#include <cmath>
 //#include <algorithm>
 using namespace std;
 
 //commit
 int main(int argc, const char * argv[]) {
-    string a, b, c;
+    double a, b, c;
     cin >> a >> b >> c;
-    if (a<b) {
-        if (a<c) {
-            cout << a << endl;
+    double Det;
+    double b2a;
+    double sqrtDet;
+    if (a==0) {
+        if (b!=0) {
+            cout << -c/b << endl;
+        } else {
+            cout << endl;
         }
-        else {
-            cout << c << endl;
-        }
-    }
-    else {
-        if (b<c) {
-            cout << b << endl;
-        }
-        else {
-            cout << c << endl;
+    } else {
+        Det=b*b-4*a*c;
+        if (Det<0) {
+            cout << endl;
+        } else {
+            b2a=-b/2/a;
+            if (Det==0) {
+                cout << b2a << endl;
+            } else {
+                sqrtDet=sqrt(Det)/2/a;
+                cout << b2a+sqrtDet << " " << b2a-sqrtDet << endl;
+            }
         }
     }
     return 0;
