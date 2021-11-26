575966fd68cd29acd0d9db72784d635ffdd87642
Danik <mevioly@gmail.com>
2021-11-26 16:36:22 +0300

    Chetvertoe zadanie

--- hlwrld/main.cpp
+++ hlwrld/main.cpp
@@ -8,37 +8,21 @@
 #include <iostream>
 //#include <vector>
 //#include <map>
-#include <string>
-#include <cmath>
+//#include <string>
+//#include <cmath>
 //#include <algorithm>
 using namespace std;
 
 //commit
 int main(int argc, const char * argv[]) {
-    double a, b, c;
-    cin >> a >> b >> c;
-    double Det;
-    double b2a;
-    double sqrtDet;
-    if (a==0) {
-        if (b!=0) {
-            cout << -c/b << endl;
+    int a, b;
+    cin >> a >> b;
+
+    if (b==0) {
+            cout <<"Impossible"<< endl;
         } else {
-            cout << endl;
+            cout << a/b << endl;
         }
-    } else {
-        Det=b*b-4*a*c;
-        if (Det<0) {
-            cout << endl;
-        } else {
-            b2a=-b/2/a;
-            if (Det==0) {
-                cout << b2a << endl;
-            } else {
-                sqrtDet=sqrt(Det)/2/a;
-                cout << b2a+sqrtDet << " " << b2a-sqrtDet << endl;
-            }
-        }
-    }
+    
     return 0;
 }
