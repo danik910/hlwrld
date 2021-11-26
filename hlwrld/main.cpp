cf3cfc27a69e770b22c60771e96b58c94b5260e2
Danik <mevioly@gmail.com>
2021-11-26 17:29:22 +0300

    Sed'moe zadanie

--- hlwrld/main.cpp
+++ hlwrld/main.cpp
@@ -15,12 +15,23 @@ using namespace std;
 
 //commit
 int main(int argc, const char * argv[]) {
-    int a, b;
-    cin >> a >> b;
-    for (int i=a;i<=b;i++) {
-        if (i % 2 == 0) {
-            cout << i << " ";
+    string x;
+    cin >> x;
+    int i=0, j=0;
+    for (auto X:x) {
+        if (X=='f') {
+            if (++i==2) {
+                cout << j <<endl;
+            }
+            
         }
+        ++j;
     }
+    if (i==0) {
+        cout << -2 <<endl;
+    } else if (i==1) {
+        cout << -1 <<endl;
+    }
+    
     return 0;
 }
