68f3010fe9ced8d8ead7d2010f9d0d8768ab93f7
Danik <mevioly@gmail.com>
2021-11-26 23:30:08 +0300

    Nedelya 2 - 2 AND 3 + CHRONO

--- hlwrld/main.cpp
+++ hlwrld/main.cpp
@@ -8,26 +8,44 @@
 #include <iostream>
 #include <vector>
 //#include <map>
-//#include <string>
+#include <string>
 //#include <cmath>
-//#include <algorithm>
+#include <algorithm>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 //commit
 
-int Factorial(int n) {
-    int f=1;
-    if (n>0) {
-        for (int i=2;i<=n;++i) {
-            f *= i;
-        }
-        return f;
+bool IsPalindrom(string s) {
+    //vector<char> s_letter_by_letter;
+    int len=s.size();
+    int median=len/2;
+    for (int i=0;i<=median;++i) {
+        if (s[i]!=s[len-1-i])
+            return 0;
     }
     return 1;
 }
 
-int main(int argc, const char * argv[]) {
-    int n;
-    cin >> n;
-    cout << Factorial(n);
+vector<string> PalindromFilter(vector<string> words, int minLength) {
+    vector<string> result;
+    for (auto w:words) {
+        if (IsPalindrom(w) && w.size()>=minLength) {
+            result.push_back(w);
+        }
+    }
+    return result;
 }
+
+int main(int argc, const char * argv[]) {
+    auto start=steady_clock::now();
+    vector<string> input={"aka","madam","toy","ejwiejwojeoq"};
+    input=PalindromFilter(input, 4);
+    //auto finish=steady_clock::now();
+    for (auto i:input)
+        cout << i << endl;
+    auto finish=steady_clock::now();
+    cout << duration_cast<microseconds>(finish-start).count() << endl;
+}
+
