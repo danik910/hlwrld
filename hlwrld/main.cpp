ef616e07c2d22c98bdf4682394aa35645fcc1e08
Danik <mevioly@gmail.com>
2021-11-30 11:59:03 +0300

    map - zadanie 1 - anagrams

--- hlwrld/main.cpp
+++ hlwrld/main.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 //#include <map>
 #include <string>
+#include <map>
 //#include <cmath>
 //#include <algorithm>
 //#include <chrono>
@@ -18,57 +19,25 @@ using namespace std;
 //commit
 const vector<int> months = {31,28,31,30,31,30,31,31,30,31,30,31};
 
+map<char, int> Letters(const string& s) {
+    map<char, int> m;
+    for (int j=0;j<s.size();j++) {
+        ++m[s[j]];
+    }
+    return m;
+}
+
 int main(int argc, const char * argv[]) {
+    
+    string s1, s2;
     int n;
     cin >> n;
-    string command;
-    int i;
-    string s;
-    vector<string> queue(31);
-    vector<int> nums(31);
-    vector<string> output;
-    vector<int> numout;
-    int next_month;
-    //vector<int> count_vec;
-    int month=0;
-    for (int m=0;m<n;m++) {
-        cin>>command;
-        if (command=="ADD") {
-            cin>>i;
-            cin>>s;
-            if (nums[i-1]>0)
-                queue[i-1]=queue[i-1] + " " + s;
-            else
-                queue[i-1]=queue[i-1] + s;
-            nums[i-1]++;
-        }
-        else if (command=="DUMP") {
-            cin>>i;
-            output.push_back(queue[i-1]);
-            numout.push_back(nums[i-1]);
-        }
-        else if (command=="NEXT") {
-            //queue_int=queue;
-            if (month==11)
-                next_month=0;
-            else
-                next_month=month+1;
-            for (int v=months[next_month]; v<months[month]; v++){
-                if (nums[months[next_month]-1]>0)
-                    queue[months[next_month]-1] += (" " + queue[v]);
-                else
-                    queue[months[next_month]-1] += (queue[v]);
-                nums[months[next_month]-1] += nums[v];
-            }
-            queue.resize(months[next_month]);
-            nums.resize(months[next_month]);
-            month=next_month;
-        }
-    }
-    int m=0;
-    for (auto numer:numout) {
-        cout << numout[m] << " " << output[m] << endl;
-        m++;
+    for (int i=0;i<n;i++) {
+        cin >> s1 >> s2;
+        if (Letters(s1)==Letters(s2))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
 }
 
