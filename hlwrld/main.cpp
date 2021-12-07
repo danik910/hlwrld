41676042ab74c37d0470e0e963413798582ed446
Danik <mevioly@gmail.com>
2021-12-07 23:42:43 +0300

    set - 1

--- hlwrld/main.cpp
+++ hlwrld/main.cpp
@@ -10,6 +10,7 @@
 //#include <map>
 #include <string>
 #include <map>
+#include <set>
 //#include <cmath>
 //#include <algorithm>
 //#include <chrono>
@@ -17,54 +18,20 @@ using namespace std;
 //using namespace std::chrono;
 
 //commit
-void PrintMap(const map<string, string>& countries) {
-for (auto j: countries)
-    cout << j.first << '/' << j.second << ' ';
-cout << endl;
-}
 
-void Toilets_for_shit(const vector<string>& toilets_shit) {
-for (auto j: toilets_shit)
-    cout << j << ' ';
-cout << endl;
-}
-
-void Buses_for_stop(const vector<string>& toilets_shit, string exclusion) {
-    if (toilets_shit.size()>1) {
-for (auto j: toilets_shit)
-    if (j!=exclusion)
-        cout << j << ' ';
-    }
-    else
-        cout << "no interchange";
-cout << endl;
-}
 
 int main(int argc, const char * argv[]) {
     
-    map<vector<string>, int> buses;
-    string command;
-    vector<string> stops;
-    
-    int n,m;
+    set<string> ss;
+    string s;
+        
+    int n;
     cin >> n;
 
-    int bus=0;
-    string stop;
     for (int i=0;i<n;i++) {
-        cin >> m;
-        stops.clear();
-        for (int j=0;j<m;j++) {
-            cin >> stop;
-            stops.push_back(stop);
-        }
-        if (buses.count(stops)==0) {
-            buses[stops]=++bus;
-            cout << "New bus " << bus << endl;
-        }
-        else
-            cout << "Already exists for " << buses[stops] << endl;
-
+        cin >> s;
+        ss.insert(s);
     }
+    cout << ss.size() << endl;
 }
 
