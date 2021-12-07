e40b53f369317f86b4c2b8e510806a44b755d9b0
Danik <mevioly@gmail.com>
2021-12-08 01:42:27 +0300

    set - 3

--- hlwrld/main.cpp
+++ hlwrld/main.cpp
@@ -21,7 +21,35 @@ using namespace std;
 
 
 int main(int argc, const char * argv[]) {
-    
+    set<set<string>> synonyms;
+    string command;
+    string word;
+    int m;
+    string s1, s2;
+    cin >> m;
+    for (int i=0;i<m;i++) {
+        cin >> command;
+        if (command=="ADD") {
+            cin >> s1 >> s2;
+            set<string> pair={s1,s2};
+            synonyms.insert(pair);
+        } else if (command=="COUNT"){
+            cin >> word;
+            int v=0;
+            for (auto& j:synonyms) {
+                if (j.count(word)>0)
+                    v++;
+            }
+            cout << v << endl;
+        } else {
+            cin >> s1 >> s2;
+            set<string> pair={s1,s2};
+            if (synonyms.count(pair)>0)
+                cout << "YES" << endl;
+            else
+                cout << "NO" << endl;
+        }
+    }
     set<string> ss;
     string s;
         
