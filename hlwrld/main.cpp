97a3d795fab1de735b7bd73b9af867b6ee26465c
Danik <mevioly@gmail.com>
2021-12-07 12:34:15 +0300

    map - countries

--- hlwrld/main.cpp
+++ hlwrld/main.cpp
@@ -17,27 +17,69 @@ using namespace std;
 //using namespace std::chrono;
 
 //commit
-const vector<int> months = {31,28,31,30,31,30,31,31,30,31,30,31};
-
-map<char, int> Letters(const string& s) {
-    map<char, int> m;
-    for (int j=0;j<s.size();j++) {
-        ++m[s[j]];
-    }
-    return m;
+void PrintMap(const map<string, string>& countries) {
+for (auto j: countries)
+    cout << j.first << '/' << j.second << ' ';
+cout << endl;
 }
+
 
 int main(int argc, const char * argv[]) {
     
-    string s1, s2;
+    map<string, string> countries;
+    string command;
+    string country;
+    string new_capital;
+    string old_capital;
+    string old_country_name, new_country_name;
+
+    
     int n;
     cin >> n;
+    
     for (int i=0;i<n;i++) {
-        cin >> s1 >> s2;
-        if (Letters(s1)==Letters(s2))
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+        cin >> command;
+        if (command=="CHANGE_CAPITAL") {
+            cin >> country >> new_capital;
+            if (countries.count(country)==0) {
+            countries[country]=new_capital;
+            cout << "Introduce new country " << country << " with capital " << new_capital << endl;
+            } else if (countries[country]==new_capital) {
+            cout << "Country " << country << " hasn't changed its capital" << endl;
+            } else {
+                old_capital=countries[country];
+                countries[country]=new_capital;
+                cout << "Country " << country << " has changed its capital from " << old_capital << " to " << new_capital << endl;
+            }
+            
+        } else if (command=="RENAME") {
+            cin >> old_country_name >> new_country_name;
+            
+            
+            // если запрос корректен и страна имеет столицу capital.
+            if (countries.count(old_country_name)!=0 && countries.count(new_country_name)==0 && old_country_name != new_country_name ) {
+                countries[new_country_name]=countries[old_country_name];
+                countries.erase(old_country_name);
+                cout << "Country " << old_country_name << " with capital " << countries[new_country_name] << " has been renamed to " << new_country_name << endl;
+            }
+            else {
+            // если новое название страны совпадает со старым, страна old_country_name не существует или страна new_country_name уже существует;
+                cout << "Incorrect rename, skip" << endl;
+            }
+        } else if (command=="ABOUT"){
+            cin >> country;
+            if (countries.count(country)==0)
+                cout << "Country " << country << " doesn't exist" << endl;
+            else
+                cout << "Country " << country << " has capital " << countries[country] << endl;
+        } else { //"DUMP"
+            if (countries.empty())
+            cout << "There are no countries in the world" << endl;
+            else {
+                PrintMap(countries);
+            }
+        }
+    
     }
 }
 
